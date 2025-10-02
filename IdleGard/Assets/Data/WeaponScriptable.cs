4c8b99e2a900eaaa603904f49b2d469385ff967d
kflores33 <159832582+kflores33@users.noreply.github.com>
2025-10-01 18:26:43 -0700

    edited variable order

--- IdleGard/Assets/Data/WeaponScriptable.cs
+++ IdleGard/Assets/Data/WeaponScriptable.cs
@@ -1,15 +1,13 @@
 using System.Collections.Generic;
 using UnityEngine;
-using static WeaponDataImport;
 
 [CreateAssetMenu(menuName = "Weapons/Weapon")]
 public class WeaponScriptable : ScriptableObject
 {
-    public string weapon_id;
-    public string wpnname;
-    public string ability_name;
-
-    public List<string> type;
-    public List<string> game;
+    public string wpnname; // to be displayed to player
+    public List<string> type; // can be of multiple types
+    public List<string> game; // can be from multiple games
+    public string weapon_id; // used as key
+    public string ability_name; // to be displayed to player
     public List<WeaponLevel> levels; // index 0 = level 1, index 1 = level 2, etc.
 }
