e446fb3b26386fdd77b63fc5459935b9b4545333
kflores33 <159832582+kflores33@users.noreply.github.com>
2025-11-06 01:22:30 -0800

    like a whole bunch of shit

---
+++ Cover_NoMoreHeroes/Assets/Scripts/TravisAnimTestScript.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+// animation events on imported animations: https://discussions.unity.com/t/adding-animation-events-on-imported-clips/663362
+public class TravisAnimTestScript : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
